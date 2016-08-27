5c4aa2f25a326016a9efeacfcd0467f8ff533df0
Sorrer <sorrer@gmail.com>
2016-08-26 20:55:02 -0400

    Libgdx Generated

---
+++ android/src/com/sorrer/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.sorrer.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sorrer.game.CoreGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CoreGame(), config);
+	}
+}
